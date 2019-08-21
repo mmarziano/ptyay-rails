734f19df9b5ed0154c76872ffb90cac148817ab6
Monica Marziano <mmarziano1@gmail.com>
2019-08-21 18:43:46 -0400

    upload profile image functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,11 @@ class UsersController < ApplicationController
         else
             @user = User.create(user_params)
             @household = @user.build_household(school_id: params[:user][:school_id])
+            byebug
+            uploaded_io = params[:person][:picture]
+            File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+            file.write(uploaded_io.read)
+            end
                 if @user.save 
                     log_in(@user)
                     render :show
@@ -36,10 +41,17 @@ class UsersController < ApplicationController
 
     end
 
-    def update 
-        @user = User.update(user_params)
-
-        redirect_to user_path(@user)
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        @user.profile_img = "/assets/" + upload
+        @household = @user.household
+        if @user.save 
+            redirect_to user_path(@user)
+        else 
+            @user.errors.full_messages.inspect
+            render :show
+        end
     end 
 
      
@@ -56,5 +68,11 @@ private
         end
     end
 
-
+    def upload
+        uploaded_file = current_user.email.to_s + params[:user][:profile_img].original_filename 
+        File.open(Rails.root.join('app', 'assets', 'images', 'uploaded_images', uploaded_file), 'wb') do |file|
+          file.write(uploaded_file) 
+        end
+        uploaded_file
+    end
 end
