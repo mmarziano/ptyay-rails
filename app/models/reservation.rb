c8d39fac2414114b47778aa0df10317004eea32d
Monica Marziano <mmarziano1@gmail.com>
2019-08-23 08:26:03 -0400

    fundraiser index page

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -7,4 +7,15 @@ class Reservation < ApplicationRecord
     
     belongs_to :fundraiser, dependent: :destroy
     belongs_to :household
+
+    def self.num_attendees(fundraiser)
+        attendees_count = 0
+        self.all.select do |r|
+            if r.fundraiser_id == fundraiser.id
+              attendees_count += r.attendees.count
+            end
+        end
+        attendees_count
+    end 
+
 end
