9d6cb264ebc41a74d855c66aaf5455c59bde7487
Monica Marziano <mmarziano1@gmail.com>
2019-08-24 17:42:35 -0400

    participation point method

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,5 +7,22 @@ class Student < ApplicationRecord
     def full_name 
         self.first_name.capitalize + " " + self.last_name.capitalize
     end 
+
+    def participation_points_by_student
+            school = self.school
+            school.fundraisers.each do |f|
+                f.reservations.each do |r|
+                    r.household.students.select do |s|
+                        s.id == self.id
+                    end 
+                @points = r.household.students.count 
+                end 
+            end  
+        @points    
+    end 
+
+    def participation_points_by_classroom(classroom)
+         
+    end 
 end
 
